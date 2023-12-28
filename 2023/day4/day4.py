44a8035fd6944c16a515607616cb02231b2955b6
Max Eisen <mjeisen@amazon.com>
2023-12-28 13:26:30 -0500

    Day 4 improvements

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -5,7 +5,7 @@ def main(part):
   lines = f.readlines()
   
   if part == 1:
-    return calculateWinnings(lines, 0, 0)
+    return calculateWinnings(lines, 0)
 
     # Non-recursive approach for part 1! (boooorrrrrinnnnnggg)
     '''
@@ -19,7 +19,7 @@ def main(part):
     '''
     
   # I won't lie... I compacted this one a lot too
-  # but it should still be somehwat readable/unpackable
+  # but it should still be somewhat readable/unpackable
   if part == 2:
     cardCopies = {i:1 for i in range(1,len(lines)+1)}
     for line in lines:
@@ -30,12 +30,12 @@ def main(part):
     return sum(cardCopies.values())
 
 # Recursive, one-liner approach for part 1!
-def calculateWinnings(lines, currLine, winnings):
-  while currLine < len(lines):
-    return(calculateWinnings(lines, currLine+1, winnings+math.floor(2**(len([num for num in [el for el in lines[currLine].replace('\n','').split(':')[1].split('|')[1].split(' ') if el != ''] if num in [el for el in lines[currLine].replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']])-1))))
-  return winnings
+def calculateWinnings(lines, winnings):
+  if len(lines) == 0:
+    return winnings
+  return(calculateWinnings(lines[1:], winnings+math.floor(2**(len([num for num in [el for el in lines[0].replace('\n','').split(':')[1].split('|')[1].split(' ') if el != ''] if num in [el for el in lines[0].replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']])-1))))
 
 if __name__ == "__main__":
   # Either part 1 or 2 of the problem
-  PART = 2
+  PART = 1
   print(main(PART))
