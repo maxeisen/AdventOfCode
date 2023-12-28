9b05120a9cfafd64a7ae6b17e05ff20fa92e5c49
Max Eisen <17mje@queensu.ca>
2023-12-27 22:47:54 -0500

    Day 4 complete

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -3,20 +3,33 @@ import math
 def main(part):
   f = open("input.txt", "r")
   lines = f.readlines()
-  return calculateWinnings(lines, 0, 0)
+  
+  if part == 1:
+    return calculateWinnings(lines, 0, 0)
 
-  # Non-recursive approach! (boooorrrrrinnnnnggg)
-  '''
-  winnings = 0
-  for line in lines:
-    winningNums = [el for el in line.replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']
-    myNums = [el for el in line.replace('\n','').split(':')[1].split('|')[1].split(' ') if el != '']
-    cardWinnings = 2**(len([num for num in myNums if num in winningNums])-1)
-    winnings += cardWinnings if cardWinnings >= 1 else 0
-  return(winnings)
-  '''
+    # Non-recursive approach for part 1! (boooorrrrrinnnnnggg)
+    '''
+    winnings = 0
+    for line in lines:
+      winningNums = [el for el in line.replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']
+      myNums = [el for el in line.replace('\n','').split(':')[1].split('|')[1].split(' ') if el != '']
+      cardWinnings = 2**(len([num for num in myNums if num in winningNums])-1)
+      winnings += cardWinnings if cardWinnings >= 1 else 0
+    return(winnings)
+    '''
+    
+  # I won't lie... I compacted this one a lot too
+  # but it should still be somehwat readable/unpackable
+  if part == 2:
+    cardCopies = {i:1 for i in range(1,len(lines)+1)}
+    for line in lines:
+      cardNum = int(''.join([el for el in line.replace('\n','').split(':')[0] if el.isnumeric()]))
+      numMatches = len([num for num in [el for el in line.replace('\n','').split(':')[1].split('|')[1].split(' ') if el != ''] if num in [el for el in line.replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']])
+      for num in range(cardNum+1, cardNum+1+numMatches):
+        cardCopies[num] += 1*cardCopies[cardNum]
+    return sum(cardCopies.values())
 
-# Recursive, one-liner approach!
+# Recursive, one-liner approach for part 1!
 def calculateWinnings(lines, currLine, winnings):
   while currLine < len(lines):
     return(calculateWinnings(lines, currLine+1, winnings+math.floor(2**(len([num for num in [el for el in lines[currLine].replace('\n','').split(':')[1].split('|')[1].split(' ') if el != ''] if num in [el for el in lines[currLine].replace('\n','').split(':')[1].split('|')[0].split(' ') if el != '']])-1))))
@@ -24,5 +37,5 @@ def calculateWinnings(lines, currLine, winnings):
 
 if __name__ == "__main__":
   # Either part 1 or 2 of the problem
-  PART = 1
+  PART = 2
   print(main(PART))
